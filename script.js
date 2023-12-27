65eec74461d390707cbdfa90a996cef33fafbf6c
Italo josé cavalcante da silva <italoc.silva21@gmail.com>
2023-12-26 23:15:31 -0300

    Commit2

---
+++ script.js
@@ -0,0 +1,13 @@
+function showm(){
+    const menuC = document.querySelector('.menuC')
+    menuC.style.display = 'flex'
+
+  }
+  function closem(){
+    const menuC = document.querySelector('.menuC')
+    menuC.style.display = 'none'
+
+  }
+
+
+   
